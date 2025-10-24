5ac51e8ffca6135c174df7e193628e790ae847f7
umernadim <itsumernadeem@gmail.com>
2025-10-24 11:52:38 +0500

    Add project files.

---
+++ inheritance/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// ******************************
+//          Inheritance  
+// ******************************
+
+Dog dog1 = new Dog();
+dog1.name = "Pluto";
+dog1.age = 3;
+dog1.breed = "Beagle";
+dog1.Eat();
+dog1.Bark();
+
+Cat cat1 = new Cat()
+{
+    name = "Whiskers",
+    age = 2,
+    color = "white"
+};
+cat1.Eat();
+cat1.Meow();
+
+
+class Animal
+{
+    public string name;
+    public int age;
+    public void Eat()
+    {
+        Console.WriteLine($"{name} is eating.");
+    }
+}
+
+class Dog : Animal
+{
+    public string breed;
+    public void Bark()
+    {
+        Console.WriteLine($"{name} is barking.");
+    }
+}
+
+class Cat : Animal
+{
+    public string color;
+    public void Meow()
+    {
+               Console.WriteLine($"{name} is meowing.");
+    }
+}
